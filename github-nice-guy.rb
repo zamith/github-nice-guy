5a05ceb1f9d76d43ad86b13db1f350f07d95b2ac
Miguel Palhas <mpalhas@gmail.com>
2014-11-11 17:59:26 +0000

    Moves mail body to an ERB template

--- github-nice-guy.rb
+++ github-nice-guy.rb
@@ -1,6 +1,7 @@
 require 'github_api'
 require 'dotenv'
 require 'mail'
+require 'erb'
 Dotenv.load
 
 class GithubNiceGuy
@@ -16,20 +17,17 @@ class GithubNiceGuy
     end
 
     github = Github.new basic_auth: ENV["GITHUB_BASIC_AUTH"]
-    issues = github.search.issues q: "type:pr is:open user:#{ENV['SEARCH_USER']}"
+    @user = ENV['SEARCH_USER']
+    @issues = github.search.issues q: "type:pr is:open user:#{@user}"
 
-    unless issues.empty?
-      mail_body = "Hello,<br><br> Here are some pull requests for #{ENV['SEARCH_USER']} that you \
-      might want to give a look to:<br><br>"
-      issues.items.map do |item|
-        mail_body += "<a href='#{item.pull_request.html_url}'>#{item.title}</a> by #{item.user.login}<br>"
-      end
-      mail_body += "<br>The Github Nice Guy."
+    unless @issues.empty?
+      template = ERB.new(File.read("mail_template.erb"))
+      mail_body = template.result(binding)
 
       Mail.new do
         from ENV['SMTP_USERNAME']
         to ENV['RECIPIENTS'].split(',')
-        subject "[Github Nice Guy] There are #{issues.total_count} open pull requests for #{ENV['SEARCH_USER']}"
+        subject "[Github Nice Guy] There are #{@issues.total_count} open pull requests for #{@user}"
 
         html_part do
           content_type 'text/html; charset=UTF-8'
